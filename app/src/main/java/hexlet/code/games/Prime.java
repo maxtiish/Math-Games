c6b811f22cbf27a685c9c954443cac8a44442b50
maxtiish <cearness@mail.ru>
2023-04-25 01:38:03 +0700

    add Prime-java

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,31 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class Prime {
+    public static void prime() {
+        var task = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+        String[][] questions = new String[3][2];
+
+        for (var count = 0; count < 3; count++) {
+            final Random random = new Random();
+            var num = random.nextInt(200);
+            questions[count][0] = Integer.toString(num);
+            questions[count][1] = "yes";
+
+            if (num < 2) {
+                questions[count][1] = "no";
+                break;
+            }
+
+            for (var i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    questions[count][1] = "no";
+                    break;
+                }
+            }
+        }
+        Engine.engine(task, questions);
+    }
+}
