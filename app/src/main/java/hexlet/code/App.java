0caf3ed3df34538a3c247ce2838fc125d189dfcb
maxtiish <cearness@mail.ru>
2023-04-25 02:18:56 +0700

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,7 +11,14 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
+        System.out.println("1 - Greet6 - Prime");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+
         System.out.print("Your choice: ");
         int num = scanner.nextInt();
 
