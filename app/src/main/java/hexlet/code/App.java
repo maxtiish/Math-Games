6fded057d93414f3f2f3129f813efb3299fd8402
maxtiish <cearness@mail.ru>
2023-05-01 21:53:43 +0700

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -18,30 +18,30 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
 
         System.out.print("Your choice: ");
         int choice = scanner.nextInt();
 
-        if (choice == GREET_NUM) {
-            Cli.greeting();
-        } else if (choice == EVEN_NUM) {
-            Even.runEven();
-        } else if (choice == CALC_NUM) {
-            Calc.runCalc();
-        } else if (choice == GCD_NUM) {
-            Gcd.runGcd();
-        } else if (choice == PROGRESSION_NUM) {
-            Progression.runProgression();
-        } else if (choice == PRIME_NUM) {
-            Prime.runPrime();
+        switch(choice) {
+            case GREET_NUM:
+                Cli.greeting();
+                break;
+            case EVEN_NUM:
+                Even.runEven();
+                break;
+            case CALC_NUM:
+                Calc.runCalc();
+                break;
+            case GCD_NUM:
+                Gcd.runGcd();
+                break;
+            case PROGRESSION_NUM:
+                Progression.runProgression();
+                break;
+            case PRIME_NUM:
+                Prime.runPrime();
+                break;
         }
-        scanner.close();
     }
 }
