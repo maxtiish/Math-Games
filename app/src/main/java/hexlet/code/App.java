fe86dd42dc42be5b5e157a5e2d30010eb11c1e0e
maxtiish <cearness@mail.ru>
2023-05-04 21:37:54 +0700

    fix App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.*;
-
+import hexlet.code.games.Gcd;
+import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
 import java.util.Scanner;
 public class App {
     static final int GREET_NUM = 1;
